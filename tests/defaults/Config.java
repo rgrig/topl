e67839c104bec54320a6df5923bfbc3eb7fdaadd
Radu Grigore <radugrigore@gmail.com>
2012-03-27 22:52:42 +0100

    Added defaults/Config.java.
    This shows how to configure topl.Checker before running a test, and
    provides a default set of settings.

---
+++ tests/defaults/Config.java
@@ -0,0 +1,7 @@
+import static topl.Property.checker;
+public class Config {
+  public static void main(String[] args) {
+    checker.verbose = true;
+    Main.main(args);
+  }
+}
---
+++ tests/twoprops/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] a) {
+    f();
+  }
+  static void f() { g(); }
+  static void g() {}
+}

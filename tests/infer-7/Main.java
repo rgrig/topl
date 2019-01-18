9190c4a313122e86ba9000ca7f484e5f5592ce0a
Radu Grigore <radugrigore@gmail.com>
2019-01-18 11:59:11 +0000

    updated infer command line
    (and some old test update)

--- tests/infer-7/Main.java
+++ tests/infer-7/Main.java
@@ -1,3 +1,5 @@
+import topl.Property;
+
 class C {
   void a() {}
   void b() {}
@@ -7,14 +9,17 @@ class C {
 public class Main {
   public static void main(String[] args) {
     C foo = new C();
+    if (foo == topl.Property.r0) {} else {}
     foo.a();
     go(foo); // should warn
   }
   static void go(C x) {
+    if (x == topl.Property.r0) {} else {}
     x.b();
     goagain(x);
   }
   static void goagain(C y) {
+    if (y == topl.Property.r0) {} else {}
     y.c();
   }
 }
